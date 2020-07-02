5c2f277b55a3dd2309b3b2247edf5d04da6a7361
asmith26 <asmith26@users.noreply.github.com>
2020-07-02 21:18:07 +0100

    Improve loss function * Rename categorical_crossentropy to log_loss (following scikit-learn). * Add more tests and different case handling (e.g. binary & multiclass), also based scikit-learn.

--- jax_toolkit/tests/test_loss.py
+++ jax_toolkit/tests/test_loss.py
@@ -3,22 +3,38 @@ from unittest.mock import MagicMock
 
 import jax.numpy as jnp
 
-from jax_toolkit.loss import (
-    LossNotCurrentlySupportedException,
-    categorical_crossentropy,
-    get_loss_function,
-    mean_squared_error,
-)
+from jax_toolkit.loss import LossNotCurrentlySupportedException, get_loss_function, log_loss, mean_squared_error
 
 
-class TestCategoricalCrossentropy(unittest.TestCase):
+class TestLogLoss(unittest.TestCase):
     def test_returns_correctly(self):
-        actual_loss = categorical_crossentropy(jnp.array([1]), jnp.array([1]))
+        actual_loss = log_loss(jnp.array([1]), jnp.array([1]))
         self.assertEqual(0, actual_loss)
-        actual_loss = categorical_crossentropy(jnp.array([1, 0]), jnp.array([0.9, 0.1]))
-        self.assertEqual(0.105360545, actual_loss)
-        actual_loss = categorical_crossentropy(jnp.array([1, 0]), jnp.array([0.1, 0.9]))
-        self.assertEqual(2.3025851, actual_loss)
+        actual_loss = log_loss(jnp.array([0]), jnp.array([0]))
+        self.assertEqual(0, actual_loss)
+        actual_loss = log_loss(jnp.array([1, 0]), jnp.array([1, 0]))
+        self.assertEqual(0, actual_loss)
+        actual_loss = log_loss(jnp.array([[0, 1], [1, 0]]), jnp.array([[0, 1], [1, 0]]))
+        self.assertEqual(0, actual_loss)
+
+        actual_loss = log_loss(jnp.array([1, 0]), jnp.array([1, 1]))
+        self.assertEqual(17.269388, actual_loss)
+
+        # Based on scikit-learnn: https://github.com/scikit-learn/scikit-learn/blob
+        # /ffbb1b4a0bbb58fdca34a30856c6f7faace87c67/sklearn/metrics/tests/test_classification.py#L2135
+        actual_loss = log_loss(jnp.array([0, 0, 0, 1, 1, 1]), jnp.array([0.5, 0.9, 0.99, 0.1, 0.25, 0.999]))
+        self.assertEqual(1.8817972, actual_loss)
+        # multiclass case
+        actual_loss = log_loss(
+            jnp.array([[0, 1, 0], [1, 0, 0], [0, 0, 1]]), jnp.array([[0.2, 0.7, 0.1], [0.6, 0.2, 0.2], [0.6, 0.1, 0.3]])
+        )
+        self.assertEqual(0.69049114, actual_loss)
+        # raise error if attempt to use not one hot encoded multiclass
+        with self.assertRaises(ValueError) as _:
+            log_loss(jnp.array([1, 0, 2]), jnp.array([[0.2, 0.7, 0.1], [0.6, 0.2, 0.2], [0.6, 0.1, 0.3]]))
+        # raise error if number of classes are not equal.
+        with self.assertRaises(TypeError) as _:
+            log_loss(jnp.array([[0, 0, 1], [0, 0, 1], [0, 0, 1]]), jnp.array([[0.2, 0.7], [0.6, 0.5], [0.4, 0.1]]))
 
 
 class TestMeanSquaredError(unittest.TestCase):
