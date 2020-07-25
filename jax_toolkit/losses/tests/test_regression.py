01470bd47f575ff4b5ee0c16ca1844de9a4cc1c4
asmith26 <asmith26@users.noreply.github.com>
2020-07-25 21:38:32 +0100

    Refactor test loss regression

--- jax_toolkit/losses/tests/test_regression.py
+++ jax_toolkit/losses/tests/test_regression.py
@@ -6,65 +6,78 @@ from jax_toolkit.losses.regression import mean_absolute_error, median_absolute_e
 
 
 class TestMeanAbsoluteError(unittest.TestCase):
-    def test_returns_correctly(self):
+    def test_single_output_returns_correctly(self):
         actual_loss = mean_absolute_error(jnp.array([0, 1, 2]), jnp.array([0, 1, 2]))
         self.assertEqual(0, actual_loss)
         actual_loss = mean_absolute_error(jnp.array([0, 1, 2]), jnp.array([0, 0, 0]))
         self.assertEqual(1, actual_loss)
-        # multi-output
-        actual_loss = mean_absolute_error(jnp.array([[0, 1, 2], [0, 1, 2]]), jnp.array([[0, 1, 2], [0, 1, 2]]))
-        self.assertEqual(0, actual_loss)
-        actual_loss = mean_absolute_error(
-            jnp.array([[1, 2, 3, 4], [4, 3, 2, 1], [1, 1, 0, 0]]), jnp.array([[1, 2, 0, 1], [4, 3, 1, 1], [0, 0, 0, 1]])
-        )
-        self.assertEqual(0.8333334, actual_loss)
         # Based on scikit-learn: https://github.com/scikit-learn/scikit-learn/blob
         # /ffbb1b4a0bbb58fdca34a30856c6f7faace87c67/sklearn/metrics/tests/test_regression.py#L25
         y_true = jnp.arange(50)
         y_pred = y_true + 1
         actual_loss = mean_absolute_error(y_true, y_pred)
         self.assertEqual(1, actual_loss)
-        # raise error if number of y_true and y_pred inputs don't match.
+
+    def test_raises_when_number_of_samples_not_equal(self):
         with self.assertRaises(TypeError) as _:
             mean_absolute_error(jnp.array([0, 0]), jnp.array([0, 0, 0]))
 
+    def test_multioutput_returns_correctly(self):
+        actual_loss = mean_absolute_error(jnp.array([[0, 1, 2], [0, 1, 2]]), jnp.array([[0, 1, 2], [0, 1, 2]]))
+        self.assertEqual(0, actual_loss)
+        actual_loss = mean_absolute_error(
+            jnp.array([[1, 2, 3, 4], [4, 3, 2, 1], [1, 1, 0, 0]]), jnp.array([[1, 2, 0, 1], [4, 3, 1, 1], [0, 0, 0, 1]])
+        )
+        self.assertEqual(0.8333334, actual_loss)
+
+    def test_raises_when_number_of_samples_not_equal_multioutput(self):
+        with self.assertRaises(TypeError) as _:
+            mean_absolute_error(jnp.array([[0, 1], [1, 2]]), jnp.array([[0.2, 0.7], [0.6, 0.5], [0.4, 0.1]]))
+
+    def test_raises_when_number_of_multioutput_outputs_not_equal(self):
+        with self.assertRaises(TypeError) as _:
+            mean_absolute_error(jnp.array([[1, 2, 3], [10, 11, 12]]), jnp.array([[0.2, 0.7], [0.6, 0.5]]))
+
 
 class TestMedianAbsoluteError(unittest.TestCase):
-    def test_returns_correctly(self):
+    def test_single_output_returns_correctly(self):
         actual_loss = median_absolute_error(jnp.array([0, 1, 2]), jnp.array([0, 1, 2]))
         self.assertEqual(0, actual_loss)
         actual_loss = median_absolute_error(jnp.array([0, 1, 2]), jnp.array([0, 0, 0]))
         self.assertEqual(1, actual_loss)
-        # multi-output
-        actual_loss = median_absolute_error(jnp.array([[0, 1, 2], [0, 1, 2]]), jnp.array([[0, 1, 2], [0, 1, 2]]))
-        self.assertEqual(0, actual_loss)
-        actual_loss = median_absolute_error(
-            jnp.array([[1, 2, 3, 4], [4, 3, 2, 1], [1, 1, 0, 0]]), jnp.array([[1, 2, 0, 1], [4, 3, 1, 1], [0, 0, 0, 1]])
-        )
-        self.assertEqual(0.5, actual_loss)
         # Based on scikit-learn: https://github.com/scikit-learn/scikit-learn/blob
         # /ffbb1b4a0bbb58fdca34a30856c6f7faace87c67/sklearn/metrics/tests/test_regression.py#L25
         y_true = jnp.arange(50)
         y_pred = y_true + 1
         actual_loss = median_absolute_error(y_true, y_pred)
         self.assertEqual(1, actual_loss)
-        # raise error if number of y_true and y_pred inputs don't match.
+
+    def test_raises_when_number_of_samples_not_equal(self):
         with self.assertRaises(TypeError) as _:
             median_absolute_error(jnp.array([0, 0]), jnp.array([0, 0, 0]))
 
+    def test_multioutput_returns_correctly(self):
+        actual_loss = median_absolute_error(jnp.array([[0, 1, 2], [0, 1, 2]]), jnp.array([[0, 1, 2], [0, 1, 2]]))
+        self.assertEqual(0, actual_loss)
+        actual_loss = median_absolute_error(
+            jnp.array([[1, 2, 3, 4], [4, 3, 2, 1], [1, 1, 0, 0]]), jnp.array([[1, 2, 0, 1], [4, 3, 1, 1], [0, 0, 0, 1]])
+        )
+        self.assertEqual(0.5, actual_loss)
+
+    def test_raises_when_number_of_samples_not_equal_multioutput(self):
+        with self.assertRaises(TypeError) as _:
+            median_absolute_error(jnp.array([[0, 1], [1, 2]]), jnp.array([[0.2, 0.7], [0.6, 0.5], [0.4, 0.1]]))
+
+    def test_raises_when_number_of_multioutput_outputs_not_equal(self):
+        with self.assertRaises(TypeError) as _:
+            median_absolute_error(jnp.array([[1, 2, 3], [10, 11, 12]]), jnp.array([[0.2, 0.7], [0.6, 0.5]]))
+
 
 class TestMaxAbsoluteError(unittest.TestCase):
-    def test_returns_correctly(self):
+    def test_single_output_returns_correctly(self):
         actual_loss = max_absolute_error(jnp.array([0, 1, 2]), jnp.array([0, 1, 2]))
         self.assertEqual(0, actual_loss)
         actual_loss = max_absolute_error(jnp.array([0, 1, 2]), jnp.array([0, 0, 0]))
-        self.assertEqual(2, actual_loss)
-        # multi-output
-        actual_loss = max_absolute_error(jnp.array([[0, 1, 2], [0, 1, 2]]), jnp.array([[0, 1, 2], [0, 1, 2]]))
-        self.assertEqual(0, actual_loss)
-        actual_loss = max_absolute_error(
-            jnp.array([[1, 2, 3, 4], [4, 3, 2, 1], [1, 1, 0, 0]]), jnp.array([[1, 2, 0, 1], [4, 3, 1, 1], [0, 0, 0, 1]])
-        )
         self.assertEqual(2, actual_loss)
         # Based on scikit-learn: https://github.com/scikit-learn/scikit-learn/blob
         # /ffbb1b4a0bbb58fdca34a30856c6f7faace87c67/sklearn/metrics/tests/test_regression.py#L25
@@ -72,30 +85,57 @@ class TestMaxAbsoluteError(unittest.TestCase):
         y_pred = y_true + 1
         actual_loss = max_absolute_error(y_true, y_pred)
         self.assertEqual(1, actual_loss)
-        # raise error if number of y_true and y_pred inputs don't match.
+
+    def test_raises_when_number_of_samples_not_equal(self):
         with self.assertRaises(TypeError) as _:
             max_absolute_error(jnp.array([0, 0]), jnp.array([0, 0, 0]))
 
+    def test_multioutput_returns_correctly(self):
+        actual_loss = max_absolute_error(jnp.array([[0, 1, 2], [0, 1, 2]]), jnp.array([[0, 1, 2], [0, 1, 2]]))
+        self.assertEqual(0, actual_loss)
+        actual_loss = max_absolute_error(
+            jnp.array([[1, 2, 3, 4], [4, 3, 2, 1], [1, 1, 0, 0]]), jnp.array([[1, 2, 0, 1], [4, 3, 1, 1], [0, 0, 0, 1]])
+        )
+        self.assertEqual(2, actual_loss)
+
+    def test_raises_when_number_of_samples_not_equal_multioutput(self):
+        with self.assertRaises(TypeError) as _:
+            max_absolute_error(jnp.array([[0, 1], [1, 2]]), jnp.array([[0.2, 0.7], [0.6, 0.5], [0.4, 0.1]]))
+
+    def test_raises_when_number_of_multioutput_outputs_not_equal(self):
+        with self.assertRaises(TypeError) as _:
+            max_absolute_error(jnp.array([[1, 2, 3], [10, 11, 12]]), jnp.array([[0.2, 0.7], [0.6, 0.5]]))
+
 
 class TestMeanSquaredError(unittest.TestCase):
-    def test_returns_correctly(self):
+    def test_single_output_returns_correctly(self):
         actual_loss = mean_squared_error(jnp.array([0, 1, 2]), jnp.array([0, 1, 2]))
         self.assertEqual(0, actual_loss)
         actual_loss = mean_squared_error(jnp.array([0, 1, 2]), jnp.array([0, 0, 0]))
         self.assertEqual(1.6666667, actual_loss)
-        # multi-output
-        actual_loss = mean_squared_error(jnp.array([[0, 1, 2], [0, 1, 2]]), jnp.array([[0, 1, 2], [0, 1, 2]]))
-        self.assertEqual(0, actual_loss)
-        actual_loss = mean_squared_error(
-            jnp.array([[1, 2, 3, 4], [4, 3, 2, 1], [1, 1, 0, 0]]), jnp.array([[1, 2, 0, 1], [4, 3, 1, 1], [0, 0, 0, 1]])
-        )
-        self.assertEqual(1.8333334, actual_loss)
         # Based on scikit-learn: https://github.com/scikit-learn/scikit-learn/blob
         # /ffbb1b4a0bbb58fdca34a30856c6f7faace87c67/sklearn/metrics/tests/test_regression.py#L25
         y_true = jnp.arange(50)
         y_pred = y_true + 1
         actual_loss = mean_squared_error(y_true, y_pred)
         self.assertEqual(1, actual_loss)
-        # raise error if number of y_true and y_pred inputs don't match.
+
+    def test_raises_when_number_of_samples_not_equal(self):
         with self.assertRaises(TypeError) as _:
             mean_squared_error(jnp.array([0, 0]), jnp.array([0, 0, 0]))
+
+    def test_multioutput_returns_correctly(self):
+        actual_loss = mean_squared_error(jnp.array([[0, 1, 2], [0, 1, 2]]), jnp.array([[0, 1, 2], [0, 1, 2]]))
+        self.assertEqual(0, actual_loss)
+        actual_loss = mean_squared_error(
+            jnp.array([[1, 2, 3, 4], [4, 3, 2, 1], [1, 1, 0, 0]]), jnp.array([[1, 2, 0, 1], [4, 3, 1, 1], [0, 0, 0, 1]])
+        )
+        self.assertEqual(1.8333334, actual_loss)
+
+    def test_raises_when_number_of_samples_not_equal_multioutput(self):
+        with self.assertRaises(TypeError) as _:
+            mean_squared_error(jnp.array([[0, 1], [1, 2]]), jnp.array([[0.2, 0.7], [0.6, 0.5], [0.4, 0.1]]))
+
+    def test_raises_when_number_of_multioutput_outputs_not_equal(self):
+        with self.assertRaises(TypeError) as _:
+            mean_squared_error(jnp.array([[1, 2, 3], [10, 11, 12]]), jnp.array([[0.2, 0.7], [0.6, 0.5]]))
