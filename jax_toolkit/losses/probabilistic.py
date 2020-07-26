3184e9d1d8c67db5171bfc4dde9cbcb95d499674
asmith26 <asmith26@users.noreply.github.com>
2020-07-26 16:12:22 +0100

    Add kuber_leibler_divergence (including doc and test).

---
+++ jax_toolkit/losses/probabilistic.py
@@ -0,0 +1,19 @@
+import jax
+import jax.numpy as jnp
+
+
+@jax.jit
+def kullback_leibler_divergence(y_true, y_pred):
+    """ Based on tensorflow: https://github.com/tensorflow/tensorflow/blob/v2.2.0/tensorflow/python/keras/losses.py
+        #L1598-L1636 """
+    # If single dimension, assume binary classification problem
+    if y_true.ndim == 1:
+        y_true = y_true[:, jnp.newaxis]
+        y_pred = y_pred[:, jnp.newaxis]
+
+    eps = 1e-15
+    y_true = jnp.clip(y_true, eps, 1)
+    y_pred = jnp.clip(y_pred, eps, 1)
+    loss = (y_true * jnp.log(y_true / y_pred)).sum(axis=1)
+    mean_loss_per_sample = jnp.average(loss)
+    return mean_loss_per_sample
